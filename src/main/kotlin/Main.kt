a2a96b9ed9e7f8959ddfca325f68ed2be95980a1
luca.camilotti <luca.camilotti@gmail.com>
2022-11-03 18:55:50 +0100

    more class examples

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -35,6 +35,11 @@
    }
    Entry point for a kotlin application: main function
 
+   fun main(args: Array<String>) {
+   ...
+   }
+
+   You can omit {} for functions with only one instruction in the body.
    Example:
 
    fun distance(point: Punto2D) = sqrt((point.x - this.x).pow(2) + (point.y - this.y).pow(2))
@@ -87,27 +92,112 @@
    Variables
    _________
    var is a variable (mutable)
-   val is a constant
+   val is a constant (immutable)
 
    Data Type:
    Byte, Short (16 bit), Int, Long, Float, Double, Boolean
 
    Note that everything is an object in Kotlin (even Java primitive types)!
 
+   Encapsulation/Information Hiding
+   ________________________________
+
+   In Kotlin, everything inside a class is public by default.
+   So, writing "public" in front of an attribute or a function (method)
+   is redundant.
+
+   Getters and Setters
+   ___________________
+
+   In Kotlin, a property (attribute) doesn’t require explicit getter or setter methods.
+   When you define an attribute inside a class, default getter and setter methods are
+   automatically generated.
+   For example, writing:
+
+   var name: String = "Pippo"
+
+   generates default getter and setter, and it's exactly the same as writing:
+
+   var name: String = "Pippo"
+   get() {
+      return field
+   }
+   set(value) {
+      field = value
+   }
+
+   With getter and setter you will be able to read and assign the attribute
+   "name" outside the class:
+
+   myobj.name="Pluto"
+   println(myobj.name)
+
+   Of course you can modify the default getter and setter behaviour:
+
+   var name: String = "Pippo"
+   get() {
+      return "Mr."+ field
+   }
+   set(value) {
+      if(value.equals(""))
+          field = "unknown"
+      else
+          field = value
+   }
+
+   If an attribute is declared with val keyword, it is immutable (you can't use the setter on it).
+
+   Static methods in Kotlin
+   ________________________
+
+   In order to declare a method as static, you will use the "companion object" block.
+
+   // In Java:
+   class Foo {
+        public static int a() { return 1; }
+   }
+
+   // In Kotlin:
+   class Foo {
+        companion object {
+            fun a() : Int = 1
+        }
+   }
+
+   Inheritance and final keyword
+   _____________________________
+
+   Classes marked as final cannot be extended (by default all classes are final in Kotlin).
+
+   Methods marked as final cannot be overridden (by default all methods are final default in Kotlin).
+
+   To make a class a base class, mark it with "open" keyword.
+   To make a method overridable, mark it with "open" keyword.
+
 
  */
 import java.time.LocalDate
-// Simple empty class
+
+// Example 1
+// Simple empty class (no attributes, no methods):
 class Pippo
-// Class with primary constructor (generate automatically attributes and getters)
-// var attribute: cannot be modified (no setter)
+
+
+// Example 2
+// Class with primary constructor (generate automatically attributes and getters):
 class Person(val givenName: String, val familyName: String, val dateOfBirth: LocalDate)
 // you can put extra code for constructor inside init{} block
+// val attribute: cannot be modified, so you cannot use the setter
 
-// a data class is a class for holding data: it has automatically generated methods
+// Example 3
+// A data class is a class for holding data: it has automatically generated methods
 // (toString, copy, equals, componentN(), ..)
 data class User(val name: String, val age: Int)
 
+// Example 4
+// In this class we define custom getter and setter for attribute "type".
+// We also define the init{} block to provide further operations to be executed
+// by the primary constructor.
 // var attribute: generate getter and setter
 class Animal(type: String, var breed: String) {
 
@@ -115,23 +205,43 @@ class Animal(type: String, var breed: String) {
         if(breed.equals(""))
             breed = "unknown"
     }
-    // getter and setter for field in primary constructor
-    var type: String = type  // type is public by default
+    // Getter and setter declaration for field "type" in primary constructor.
+    // Note that, in this case, the attribute is not defined inside the primary
+    // contructor (var/val keyword is missing for "type"), since it is defined here.
+    // Also note that getter and setter behaviours are the default, so their definition may be
+    // omitted (they are redundant).
+    var type: String = type  // an attribute is public by default
     get() = field
     set(value) {
         field = value
     }
 
-    // Define additional attribute with getter and setter:
-    private var age: Int = 0  // if private, you can't use getter and setter..
+    // Define additional "age" attribute with custom setter (when you don't define a getter, the default
+    // one is generated).
+    private var age: Int = 0  // if private, you can't use getter and setter outside the class..
     set(value) {  // setter for age
         if(value > 0)
             field = value
         else
             field = 0
     }
-    // Secondary constructor
+
+    // Define a method:
+    fun getAge(): Int {
+        return this.age; // return the value of the private attribute
+    }
+
+    // Define a secondary constructor:
+    // Note that this secondary constructor uses the primary constructor
+    // to set type and breed attributes: this(type, breed).
+    // Calling the primary contructor in a secondary constructor is mandatory.
     constructor(type : String , breed :String, age :Int):this(type,breed) {
+        if(age > 0)
+            this.age = age
+    }
+
+    // Define another secondary constructor:
+    constructor(type : String) : this(type, "") {
         if(age > 0)
             this.age = age
     }
@@ -188,7 +298,9 @@ fun main(args: Array<String>) {
 
     println("Breed: "+animal.breed)
     animal.type = "monkey";  // setter
+    animal.breed = "macaco";
     // animal.age = 5;  // Error! age attribute is private
+    println("This "+animal.type+" is "+animal.getAge()+" years old.")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
